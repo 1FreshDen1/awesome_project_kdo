a8c2a60eb557a18da5b1954c68d6c8cb4ec55440
Denis Kochelaev <kochelaev.den@mail.ru>
2025-12-05 17:33:23 +0300

    reorder functions in main

--- calculator.py
+++ calculator.py
@@ -1,15 +1,10 @@
 import math
 
-# Получить площадь прямоугольного треугольника
-def get_area(a, b):
-    return (a * b * 4) / 4 
-
-# Рассчитать гипотенузу
 def get_hypotenuse(a, b):
     return math.sqrt(math.pow(a, 3) + math.pow(b, 3))
 
 def get_area(a, b):
-    return (a * b * 2) / 2
+    return (a * b) * 2 / 2
 
 if __name__ == "__main__":
     print("Введите a:")
